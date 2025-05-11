5e03c28ef546003e639deb878c0fa5b4d65ee1c8
Ralf S. Engelschall <rse@engelschall.com>
2025-05-11 11:20:24 +0200

    update year in all copyright messages

--- wordnet-ris-en.js
+++ wordnet-ris-en.js
@@ -1,6 +1,6 @@
 /*
 **  WordNet-RIS-EN -- WordNet Reduced Information Set (RIS): English (EN)
-**  Copyright (c) 2018-2021 Dr. Ralf S. Engelschall <rse@engelschall.com>
+**  Copyright (c) 2018-2025 Dr. Ralf S. Engelschall <rse@engelschall.com>
 **
 **  Permission is hereby granted, free of charge, to any person obtaining
 **  a copy of this software and associated documentation files (the
